94a5b01364e0a498be87fdaf89ddda5995169a53
Areyall <anhetec@gmail.com>
2024-12-12 09:41:02 +0200

    patch

--- src/components/sections/subSection/about/AboutSection.tsx
+++ src/components/sections/subSection/about/AboutSection.tsx
@@ -7,7 +7,7 @@ export function AboutSection({ aboutMeData }: { aboutMeData: IWhatIDo }) {
   return (
     <MainSection id='about' title={aboutMeData.title} className="relative">
       <AboutMeSubSection aboutMeData={aboutMeData} />
-      <div className="z-[-1] absolute top-[104px] -left-4  w-14 h-14 rounded-sm bg-secondary"></div>
+      <div className="z-[-1] absolute top-[75px] xl:top-[104px] -left-4  w-14 h-14 rounded-sm bg-secondary"></div>
       <WhatIDoSubSection aboutMeData={aboutMeData} />
     </MainSection>
   );

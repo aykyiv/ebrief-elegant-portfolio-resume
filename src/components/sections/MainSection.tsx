8cc521312074834b877d531af9d6c966d5899320
Areyall <anhetec@gmail.com>
2024-12-13 18:18:58 +0200

    patch

--- src/components/sections/MainSection.tsx
+++ src/components/sections/MainSection.tsx
@@ -4,7 +4,7 @@ import { IMainSectionProps } from "@/interfaces/IWhatIDo";
 export function MainSection({ title, children, className, id }: IMainSectionProps) {
   return (
     <section id={id} className={`w-full max-w-4xl mx-auto flex flex-col gap-7 xl:py-8 ${className}`}>
-      <div className="flex flex-row items-center ">
+      <div className="flex flex-row items-center  gap-4">
         <h1 className=" font-semibold text-primary min-w-max">
           {title}
         </h1>
